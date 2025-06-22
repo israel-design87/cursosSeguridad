b018a0b963701698197e1a7bf8a29fb4a1b15881
Landevel <2520160008ilanderosc@gmail.com>
2025-06-21 20:53:01 -0600

    modificacion settings 4

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
-set -o errexit
+set -o errexit  # para que el script falle si algún comando falla
+set -o pipefail
+set -o nounset
 
 echo "Instalando dependencias..."
 pip install -r requirements.txt
 
 echo "Aplicando migraciones..."
-python manage.py migrate
+python manage.py migrate --no-input
 
 echo "Recolectando archivos estáticos..."
 python manage.py collectstatic --no-input
 
-echo "Build terminado correctamente."
+echo "Build y migraciones completadas correctamente."
